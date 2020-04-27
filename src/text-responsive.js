5ca78da695be8943de39b0c44ca61559b13d7f28
Ade Ayu Safitri <deyusf12@gmail.com>
2020-04-28 05:08:50 +0700

    Fixed text responsive

--- src/text-responsive.js
+++ src/text-responsive.js
@@ -1,16 +1,16 @@
 import { scaleHeight } from "../lib/responsive";
 
 const textResponsive = {
-  "text-r-xs": scaleHeight('1.42%'),
-  "text-r-sm": scaleHeight('1.64%'),
-  "text-r-base": scaleHeight('1.84%'),
-  "text-lg": scaleHeight('2.14%'),
-  "text-xl": scaleHeight('2.32%'),
-  "text-2xl": scaleHeight('2.6%'),
-  "text-3xl": scaleHeight('2.8%'),
-  "text-4xl": scaleHeight('3.05%'),
-  "text-5xl": scaleHeight('3.25%'),
-  "text-r-6xl": scaleHeight('3.55%')
+  "text-r-xs": { fontSize: scaleHeight('1.42%') },
+  "text-r-sm": { fontSize: scaleHeight('1.64%') },
+  "text-r-base": { fontSize: scaleHeight('1.84%') },
+  "text-r-lg": { fontSize: scaleHeight('2.14%') },
+  "text-r-xl": { fontSize: scaleHeight('2.32%') },
+  "text-r-2xl": { fontSize: scaleHeight('2.6%')},
+  "text-r-3xl": { fontSize: scaleHeight('2.8%') },
+  "text-r-4xl": { fontSize: scaleHeight('3.05%') },
+  "text-r-5xl": { fontSize: scaleHeight('3.25%') },
+  "text-r-6xl": { fontSize: scaleHeight('3.55%') }
 }
 
 export default textResponsive
