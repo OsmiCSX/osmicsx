f762833c2f838da465a39583a1dffd2df1976c71
DeVoresyah ArEst <de.voresyah@gmail.com>
2021-06-28 11:44:46 +0700

    chore: remove dist output

--- dist/processor/darkThemeProcessor.d.ts
+++
@@ -1,3 +0,0 @@
-import { BackgroundDark, BorderDark, TextDark } from "./processor.type";
-declare const _default: (styles: object, bgDark: BackgroundDark | undefined, borderDark: BorderDark | undefined, textDark: TextDark | undefined) => object;
-export default _default;
